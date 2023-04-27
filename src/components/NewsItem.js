7b138887979c66f81d980dcb89ea2f04d1e5a6e1
Rajin Sakha <rajinsakha07@gmail.com>
2023-04-27 21:07:44 +0545

    Sending build

---
+++ src/components/NewsItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+ 
+
+  render() {
+    let {title, description,imageUrl, author, date, newsUrl, source} = this.props;
+    return (
+      <div className="container my-3">
+         <div className="card">
+          <div style={{display:'flex', justifyContent:'flex-end', position:'absolute', right:0, top: '-0.50rem'}}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+        <img src={imageUrl}className="card-img-top" alt="..."/>
+        <div className="card-body">
+        <h5 className="card-title">{title} </h5>
+        <p className="card-text">{description}</p>
+        <p className="card-text"><small>By {author} on {new Date(date).toGMTString()}</small></p>
+        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
+      </div>
+      </div>
+      </div>
+    ) 
+  } 
+}
+
+export default NewsItem
